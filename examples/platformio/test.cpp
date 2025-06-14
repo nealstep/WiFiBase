a16cb0090923adfa1ea05a31737c724f5eb4982f
Neal Stephenson <neal@bakerst.org>
2025-06-14 08:22:13 -0400

    move to management in platformio

---
+++ examples/platformio/test.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#include "WiFiBase.h"
+
+WiFiBase wifiBase;
+
+static const char *ssid = MY_SSID;
+static const char *passwd = MY_PASSWD;
+
+static uint16_t delay_loop = 5000;
+
+void setup() {
+  Serial.begin(MY_BAUD);
+  Serial.println("Starting");
+  if (!wifiBase.init(ssid, passwd)) {
+    Serial.println("Failed to Connect");
+  } else {
+    Serial.print("Connected IP: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void loop() {
+  Serial.print("Wifi Connected: ");
+  Serial.println(wifiBase.connected());
+  Serial.print("Connected IP: ");
+  Serial.println(WiFi.localIP());
+  delay(delay_loop);
+}
